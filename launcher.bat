b90d46c9117110e05ad15b8b5820fb828806e448
Exponent <103051123+Rdmo1@users.noreply.github.com>
2022-06-01 03:17:57 +0800

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -3,6 +3,6 @@ echo [40;32mInstalling dependencies....[40;37m
 echo.
 pip install --upgrade -r requirements.txt
 echo.
-echo [40;32mLauncing Generator..[40;37m
+echo [40;32mLaunching Tool..[40;37m
 timeout 2 >nul
 python main.py
