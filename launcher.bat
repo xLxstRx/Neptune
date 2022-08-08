d5aa395151444cf687eb41c1319431703c594d53
Exponent <103051123+Athena-zip@users.noreply.github.com>
2022-08-08 08:00:23 +0800

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,8 +1,5 @@
-@echo off
-echo [40;32mInstalling dependencies....[40;37m
-echo.
-pip install --upgrade -r requirements.txt
-echo.
-echo [40;32mLaunching Tool..[40;37m
-timeout 2 >nul
-python main.py
+python -m pip install -r requirements.txt
+cls
+echo python main.py >> start.bat
+start start.bat
+start /b "" cmd /c &exit /b
