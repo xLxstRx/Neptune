2ffc44e3421c9f2c6aab8d60c1d611fb9430256f
Exponent <103051123+Rdmo1@users.noreply.github.com>
2022-09-04 11:00:05 +0800

    config.yml

--- config.js
+++ config.js
@@ -3,12 +3,11 @@
     "discord_id": "ENTER YOUR DISCORD ID",
     "server_id": "ENTER YOUR SERVER ID",
     
- # modify this if you want
-    "grab_webcam": true, 
-    "grab_ip": true,
-    "grab_screenshot": "false",
+    "grab_webcam": "false", 
+    "grab_screenhot": "false",
+    "grab_cookies": "false",
 
-    "grab_password": "true",
+    "grab_epic": "false",
     "grab_minecraft": "false",
     "grab_roblox": "false",
 }
